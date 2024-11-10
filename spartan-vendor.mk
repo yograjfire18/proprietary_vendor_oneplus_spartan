50abbf9ba48a3687c161ff616c3d615736934c2c
yograjfire18 <yograjfire18@gmail.com>
2024-11-10 19:43:45 +0530

    spartan: Initial setup

---
+++ spartan-vendor.mk
@@ -0,0 +1,107 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/oneplus/spartan
+
+PRODUCT_COPY_FILES += \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Bluetooth_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/General_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/General_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/Global_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Global_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/Handset_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Handset_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/Hdmi_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Hdmi_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/Headset_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Headset_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/Bluetooth_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/General_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/General_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/Global_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/Global_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/Handset_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/Handset_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/Hdmi_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/Hdmi_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/Headset_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/Headset_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/MOS/Speaker_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MOS/Speaker_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/acdbdata/Speaker_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Speaker_cal.acdb \
+    vendor/oneplus/spartan/proprietary/odm/etc/camera/ashdr/ncf_pack.ncf:$(TARGET_COPY_OUT_ODM)/etc/camera/ashdr/ncf_pack.ncf \
+    vendor/oneplus/spartan/proprietary/odm/etc/camera/darksight/darksight_main.bin:$(TARGET_COPY_OUT_ODM)/etc/camera/darksight/darksight_main.bin \
+    vendor/oneplus/spartan/proprietary/odm/etc/camera/darksight/darksight_ultrawide.bin:$(TARGET_COPY_OUT_ODM)/etc/camera/darksight/darksight_ultrawide.bin \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_bu52053nvx_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_bu52053nvx_0.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_ak991x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_ak991x_0.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_alsps.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_alsps.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_bmi26x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_bmi26x_0.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_cct.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_cct.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_cct_rear.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_cct_rear.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_icm4x6xa.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_icm4x6xa.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_icm4x6xx.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_icm4x6xx.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_lsm6ds3c.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_lsm6ds3c.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_lsm6dso.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_lsm6dso.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_mmc5603nj.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_mmc5603nj.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_mn29005.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_mn29005.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/config/kona_qrd_sx933x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_sx933x_0.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/sensor/sensor_config.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/sensor_config.json \
+    vendor/oneplus/spartan/proprietary/odm/etc/wifi/bdwlan.elf:$(TARGET_COPY_OUT_ODM)/etc/wifi/bdwlan.elf \
+    vendor/oneplus/spartan/proprietary/odm/firmware/fastchg/21623/charging_thermal_config_default.txt:$(TARGET_COPY_OUT_ODM)/firmware/fastchg/21623/charging_thermal_config_default.txt \
+    vendor/oneplus/spartan/proprietary/odm/firmware/fastchg/21623/oplus_vooc_fw_rk826.bin:$(TARGET_COPY_OUT_ODM)/firmware/fastchg/21623/oplus_vooc_fw_rk826.bin \
+    vendor/oneplus/spartan/proprietary/odm/firmware/fastchg/charging_thermal_config_default.txt:$(TARGET_COPY_OUT_ODM)/firmware/fastchg/charging_thermal_config_default.txt \
+    vendor/oneplus/spartan/proprietary/odm/firmware/fastchg/smart_chg_config_table.txt:$(TARGET_COPY_OUT_ODM)/firmware/fastchg/smart_chg_config_table.txt \
+    vendor/oneplus/spartan/proprietary/odm/firmware/tfa98xx.cnt:$(TARGET_COPY_OUT_ODM)/firmware/tfa98xx.cnt \
+    vendor/oneplus/spartan/proprietary/odm/firmware/tp/21623/FW_FT3658U_SAMSUNG.img:$(TARGET_COPY_OUT_ODM)/firmware/tp/21623/FW_FT3658U_SAMSUNG.img \
+    vendor/oneplus/spartan/proprietary/odm/firmware/tp/21623/FW_FT3658U_SAMSUNG_FAE.img:$(TARGET_COPY_OUT_ODM)/firmware/tp/21623/FW_FT3658U_SAMSUNG_FAE.img \
+    vendor/oneplus/spartan/proprietary/odm/firmware/tp/21623/LIMIT_FT3658U_SAMSUNG.img:$(TARGET_COPY_OUT_ODM)/firmware/tp/21623/LIMIT_FT3658U_SAMSUNG.img \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.sensormodule.qtech_ov64b.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.qtech_ov64b.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.sensormodule.shine_ov02b.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.shine_ov02b.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.sensormodule.truly_hi846.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.truly_hi846.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.sensormodule.truly_imx471_21732.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.truly_imx471_21732.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.tuned.sm8250_qtech_ov64b.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.sm8250_qtech_ov64b.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.tuned.sm8250_shine_ov02b.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.sm8250_shine_ov02b.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.tuned.sm8250_truly_hi846.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.sm8250_truly_hi846.bin \
+    vendor/oneplus/spartan/proprietary/odm/lib64/camera/com.qti.tuned.sm8250_truly_imx471_21732.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.sm8250_truly_imx471_21732.bin \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b00:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b00 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b01:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b01 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b02:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b02 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b03:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b03 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b04:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b04 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b05:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b05 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b06:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b06 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.b07:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.b07 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/ancap64.mdt:$(TARGET_COPY_OUT_ODM)/vendor/firmware/ancap64.mdt \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b00:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b00 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b01:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b01 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b02:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b02 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b03:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b03 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b04:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b04 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b05:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b05 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b06:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b06 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.b07:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.b07 \
+    vendor/oneplus/spartan/proprietary/odm/vendor/firmware/goodixfp.mdt:$(TARGET_COPY_OUT_ODM)/vendor/firmware/goodixfp.mdt \
+    vendor/oneplus/spartan/proprietary/vendor/etc/init/android.hardware.keymaster@4.1-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.keymaster@4.1-service-qti.rc \
+    vendor/oneplus/spartan/proprietary/vendor/etc/ltm_config_samsung_ams662zs01_dsc_cmd_21623.xml:$(TARGET_COPY_OUT_VENDOR)/etc/ltm_config_samsung_ams662zs01_dsc_cmd_21623.xml \
+    vendor/oneplus/spartan/proprietary/vendor/etc/qdcm_calib_data_samsung_ams662zs01_dsc_cmd_21623.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_samsung_ams662zs01_dsc_cmd_21623.xml \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b00 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b01 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b02 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b03:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b03 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b04:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b04 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b05:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b05 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b06:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b06 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.b07:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.b07 \
+    vendor/oneplus/spartan/proprietary/vendor/firmware/image/opluseid.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/image/opluseid.mdt
+
+PRODUCT_PACKAGES += \
+    vendor.oplus.hardware.cameraextension-V1-ndk_platform \
+    anc.hal \
+    com.qti.sensor.hi846 \
+    com.qti.sensor.imx471 \
+    com.qti.sensor.ov02b \
+    com.qti.sensor.ov64b \
+    fingerprint.goodix_G2.default \
+    fingerprint.goodix_G3.default \
+    fingerprint.goodix_G5.default \
+    fingerprint.goodix_G6.default \
+    fingerprint.goodix_G7.default \
+    libgf_hal_G2 \
+    libgf_hal_G3 \
+    libgf_hal_G5 \
+    libgf_hal_G6 \
+    libgf_hal_G7 \
+    libhwconfigurationutil \
+    vendor.oplus.hardware.cameraextension-V1-service-impl \
+    android.hardware.keymaster@4.1-service-qti
